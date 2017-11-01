9eaac6720ec0c70e63ac02b0821d39d80e01991f
tylermoore1 <32109556+tylermoore1@users.noreply.github.com>
2017-10-31 18:07:16 -0600

    Sql file uploaded to make database

---
+++ mysql/InsertDatabase.sql
@@ -0,0 +1,13 @@
+drop table users;
+drop table questions;
+drop table answers;
+
+create table users (id integer primary key, email char(40), username char(20));
+create table questions (id integer primary key, message char(255), subject char(16), score integer, user char(20) references users(username));
+create table answers (id integer primary key, question_id integer references questions(id), message char(255), score integer, user char(20) references users(username));
+
+insert into users values (1, "tymo1918@colorado.edu", "tylermoore");
+
+insert into questions values (1, "What''s 2 + 2?", "Math", 5, "tylermoore");
+
+insert into answers values (1, 1, "The answer is 4", 10, "tylermoore");
