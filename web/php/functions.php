74b04c51cd340878fc00a7c45ed9fb6dc2a97646
Tyler Moore <tymo1918@colorado.edu>
2017-12-02 12:41:33 -0700

    Added test files

--- web/php/functions.php
+++ web/php/functions.php
@@ -14,14 +14,35 @@ function sqlConnect(){
 		$server = "localhost";
 		$username = "root";
 		$password = "";
-		$db = "connectcu";
+		$db = "ConnectCU";
 	}
-	
+
 	$conn = new mysqli($server, $username, $password, $db);
-	
+
 	if ($conn->connect_error){
 		die('Could not connect: ' . $conn->connect_error);
 	}
-	
+
 	return $conn;
 }
+
+function delete()
+{
+  $db = sqlConnect();
+  if ($db->query('DELETE FROM questions') === FALSE){
+		return false;
+	}
+  return true;
+}
+
+function add($subject, $question, $user)
+{
+	$db = sqlConnect();
+	if ($db->query("INSERT INTO questions (message, subject, score, user) VALUES ('$question', '$subject', '0', '$user')") === FALSE){
+		return false;
+	}
+	return true;
+}
+
+
+?>
