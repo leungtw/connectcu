7850b87e035b4fb9c77dadf5e308d2d280ba8e7a
leungtw <tale9912@colorado.edu>
2017-12-04 15:45:35 -0700

    signup duplicate check

--- web/php/signupHandler.php
+++ web/php/signupHandler.php
@@ -5,7 +5,19 @@
 	$returnPage = $_POST['Return'];
 	$email = $_POST['email'];
 	$username = $_POST['username'];
-	$sql = "INSERT INTO users (email, username) VALUES ('$email', '$username');";
+
+	$duplicateUser = "SELECT username from users WHERE username = '$username';";
+	$duplicateEmail = "SELECT email from users WHERE email = '$email';";
+
+	if($duplicateEmail){
+		echo "Email already exists"
+	} elseif ($duplicateUser) {
+		echo "Username already exists"
+	}
+	else{
+		$sql = "INSERT INTO users (email, username) VALUES ('$email', '$username');";
+	}
+
 	
 	if ($conn->query($sql)===FALSE){
 		die('Error!');
