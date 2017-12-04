f63a0581070b02fcbb0da44509979afd326636ee
leungtw <tale9912@colorado.edu>
2017-12-04 15:51:43 -0700

    email check fix

--- web/SignUp.php
+++ web/SignUp.php
@@ -77,7 +77,7 @@ button {
 <form action="php/signupHandler.php" style="border:1px solid #ccc" method = "post">
   <div class="container">
     <label><b>Email</b></label>
-    <input name="email" type="text" placeholder="Enter Email"  pattern = "/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/" title = "Enter a @colorado.edu email" required>
+    <input name="email" type="text" placeholder="Enter Email"  type = "email" pattern = "/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/" title = "Enter a @colorado.edu email" required>
 
     <label><b>Username</b></label>
     <input name="username" type="text" placeholder="Enter Username" required>
