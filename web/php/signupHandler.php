711b76630b3089665332e69e3899142d0779ac8f
leungtw <tale9912@colorado.edu>
2017-12-04 18:03:38 -0700

    mysqli

--- web/php/signupHandler.php
+++ web/php/signupHandler.php
@@ -6,12 +6,12 @@
 	$email = $_POST['email'];
 	$username = $_POST['username'];
 
-	$duplicateUser = mysql_query("SELECT username FROM users WHERE username = '$username'");
-	$duplicateEmail = mysql_query("SELECT email FROM users WHERE email = '$email'");
+	$duplicateUser = mysqli_query("SELECT username FROM users WHERE username = '$username'");
+	$duplicateEmail = mysqli_query("SELECT email FROM users WHERE email = '$email'");
 
-	if(mysql_num_rows($duplicateEmail) != 0){
+	if(mysqli_num_rows($duplicateEmail) != 0){
 		echo "Email already exists!";
-	} elseif (mysql_num_rows($duplicateUser) != 0) {
+	} elseif (mysqli_num_rows($duplicateUser) != 0) {
 		echo "Username already exists!";
 	}
 	else{
