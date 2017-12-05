2c47e7d06c59b280f477020623ae36549b822c41
leungtw <tale9912@colorado.edu>
2017-12-04 17:58:28 -0700

    email duplicate check

--- web/php/signupHandler.php
+++ web/php/signupHandler.php
@@ -6,12 +6,12 @@
 	$email = $_POST['email'];
 	$username = $_POST['username'];
 
-	$duplicateUser = mysql_query("SELECT username from users WHERE username = '$username'");
-	$duplicateEmail = mysql_query("SELECT email from users WHERE email = '$email'");
+	$duplicateUser = mysql_query("SELECT username FROM users WHERE username = '$username'");
+	$duplicateEmail = mysql_query("SELECT email FROM users WHERE email = '$email'");
 
-	if(mysql_num_rows($duplicateEmail) > 0){
+	if(mysql_num_rows($duplicateEmail) != 0){
 		echo "Email already exists!";
-	} elseif (mysql_num_rows($duplicateUser) > 0) {
+	} elseif (mysql_num_rows($duplicateUser) != 0) {
 		echo "Username already exists!";
 	}
 	else{
