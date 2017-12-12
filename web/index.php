b2622fb1a968636eca2f856a9cc8a567c2e43cb7
Braden Holmes <32109818+bradenh1@users.noreply.github.com>
2017-12-12 11:33:34 -0700

    Fixed whatever chaos just happened

--- web/index.php
+++ web/index.php
@@ -32,7 +32,6 @@
 			<?php
 				if (isset($_SESSION['login_user'])){
 					echo '
-						<li>' $_SESSION['login_user']' </li>
 						<li class="button_1"> <a href="php/logout.php">Sign out</a> </li>
 					';
 				} else {
